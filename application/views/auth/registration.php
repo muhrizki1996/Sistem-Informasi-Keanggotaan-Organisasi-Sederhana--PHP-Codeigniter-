611c6646cc26c0dea27fd79eba8908d25f8129bd
Muhammad Rixki <kakarizki@icloud.com>
2019-07-23 12:26:36 +0800

    Fix error message color on Registraion page

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -8,14 +8,14 @@
                         <div class="col-md-6">
                             <div class="form-label-group">
                                 <input type="text" id="firstname" name="firstname" class="form-control" placeholder="First name" required="required" autofocus="autofocus" value="<?= set_value('firstname'); ?>">
-                                <?= form_error('firstname'); ?>
+                                <?= form_error('firstname', '<small class="text-danger">', '</small>'); ?>
                                 <label for="firstname">First name</label>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-label-group">
                                 <input type="text" id="lastname" name="lastname" class="form-control" placeholder="Last name" required="required" value="<?= set_value('lastname'); ?>">
-                                <?= form_error('lastname'); ?>
+                                <?= form_error('lastname', '<small class="text-danger">', '</small>'); ?>
                                 <label for="lastname">Last name</label>
                             </div>
                         </div>
@@ -24,7 +24,7 @@
                 <div class="form-group">
                     <div class="form-label-group">
                         <input type="text" id="email" name="email" class="form-control" placeholder="Email address" required="required" value="<?= set_value('email'); ?>">
-                        <?= form_error('email'); ?>
+                        <?= form_error('email', '<small class="text-danger">', '</small>'); ?>
                         <label for="email">Email address</label>
                     </div>
                 </div>
@@ -33,14 +33,14 @@
                         <div class="col-md-6">
                             <div class="form-label-group">
                                 <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="required">
-                                <?= form_error('password'); ?>
+                                <?= form_error('password', '<small class="text-danger">', '</small>'); ?>
                                 <label for="password">Password</label>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-label-group">
                                 <input type="password" id="confirmpassword" name="confirmpassword" class="form-control" placeholder="Confirm Password" required="required">
-                                <?= form_error('confirmpassword'); ?>
+                                <?= form_error('confirmpassword', '<small class="text-danger">', '</small>'); ?>
                                 <label for="confirmpassword">Confirm password</label>
                             </div>
                         </div>
